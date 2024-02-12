2fcc041d824f4c1b09e2061d146802ed840db9fb
Rhys Spano <rhysspano@Rhyss-MacBook-Pro-2.local>
2024-02-12 10:26:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mr.-Manatee-the-Manatee
---
+++ manatee download/script.js
@@ -0,0 +1,8 @@
+let manateeHeader = document.getElementById("manatee-header");
+manateeHeader.addEventListener("click", changeBackground);
+
+function changeBackground () {
+    document.getElementById("body-background").style.backgroundColor = "purple";
+    console.log("wuuush");
+
+}
